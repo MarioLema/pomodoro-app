5c52b01a60d60d2448b7c859d9941de56b5eecb8
Mario Lema <lema_mario@yahoo.com>
2019-02-21 16:34:54 +0000

    Updates

---
+++ precache-manifest.a1037a808d8d7bc4386b91a2c516dd9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5e48a8eee39fe3792fb8de23c4f1db0",
+    "url": "/pomodoro-app/static/media/birds.c5e48a8e.mp3"
+  },
+  {
+    "revision": "1dcdf904dbc3dce338d5",
+    "url": "/pomodoro-app/static/js/runtime~main.1dcdf904.js"
+  },
+  {
+    "revision": "6ac4469af878aa7e0129",
+    "url": "/pomodoro-app/static/js/main.6ac4469a.chunk.js"
+  },
+  {
+    "revision": "77dc30198dc5cd0dc996",
+    "url": "/pomodoro-app/static/js/2.77dc3019.chunk.js"
+  },
+  {
+    "revision": "6ac4469af878aa7e0129",
+    "url": "/pomodoro-app/static/css/main.fc3e802a.chunk.css"
+  },
+  {
+    "revision": "e21d46ddf6a59e8da19b184b44f683ac",
+    "url": "/pomodoro-app/index.html"
+  }
+];
